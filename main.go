8a0044354fa815becb76e1593da035d2ebce5c2c
Nicky Fusté <nicky.fuste@gmail.com>
2018-03-11 19:17:43 +0100

    News agregator tutorial

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-const pi = 3.14
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
+// I've done this using this tutorial: https://www.youtube.com/watch?v=ccANcNk8Dac
 func main() {
-	println("Pi es", pi)
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	stringBody := string(bytes)
+	fmt.Println(stringBody)
+	resp.Body.Close()
 }
