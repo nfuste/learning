8bdc45998e6f5f55ab38c44771783b7efc03739e
Nicky Fusté Massa <nicky@Nickys-MacBook-Air.local>
2018-04-14 20:50:55 +0200

    Expenses struct added

--- main.go
+++ main.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
+//Expenses will have a name and an amount
+type Expenses struct {
+	name   string
+	amount float64
+}
+
 func main() {
-	fmt.Println("Let's start again")
+	todayexpenses := Expenses{
+		name:   "meal",
+		amount: 12.5,
+	}
+
+	fmt.Println("Today you have spent:", todayexpenses.amount, "in", todayexpenses.name)
 }
