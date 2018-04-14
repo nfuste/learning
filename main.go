1eeffa24657c87d6049e0e1ad828dd0d6a8bf53f
Nicky Fusté Massa <nicky@Nickys-MacBook-Air.local>
2018-04-14 20:35:09 +0200

    Let's start again

--- main.go
+++ main.go
@@ -1,34 +1,9 @@
 package main
 
 import (
-	"encoding/xml"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 )
 
-// I've done this using this tutorial: https://www.youtube.com/watch?v=ccANcNk8Dac
-// and this one too: https://www.youtube.com/watch?v=-PATP8IZq5A
-
-type SitemapIndex struct {
-	Locations []Location `xml:"sitemap"`
-}
-
-type Location struct {
-	Loc string `xml:"loc"`
-}
-
-func (l Location) String() string {
-	return fmt.Sprintf(l.Loc)
-}
-
 func main() {
-	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
-	bytes, _ := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-
-	var s SitemapIndex
-	xml.Unmarshal(bytes, &s)
-
-	fmt.Println(s.Locations)
+	fmt.Println("Let's start again")
 }
